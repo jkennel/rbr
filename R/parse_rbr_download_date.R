4f34536a7c71d7bb865bb93e9e9ededb66dadad1
jkennel <jkennel@uoguelph.ca>
2018-11-27 10:45:52 -0500

    parse date from filename

---
+++ R/parse_rbr_download_date.R
@@ -0,0 +1,22 @@
+#' parse_rbr_download_date
+#'
+#' RBR files typically save the date in the downloaded file name.  This function
+#' parses that date to a POSIXct
+#'
+#' @param fn {character} scalar or vector of file name(s)
+#' @param tz {character} timezone
+#'
+#' @return POSIXct datetime
+#' @export
+#'
+#' @examples
+#' dt <- parse_rbr_download_date('20141117_1240')
+parse_rbr_download_date <- function(fn, tz = 'UTC') {
+
+  ind <- regexpr('\\d{8}\\_\\d{4}', index_rbr$fn_in)
+  tm <- regmatches(index_rbr$fn_in, ind)
+  tm <- as.POSIXct(tm,
+                   format = '%Y%m%d_%H%M',
+                   tz = tz)
+
+}
