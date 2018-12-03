fdb00999f75eeb467ca2bf1e4c6b81e9a671b095
jkennel <jkennel@uoguelph.ca>
2018-12-03 16:27:44 -0500

    handle NA in POSIXct

--- R/parse_rbr_download_date.R
+++ R/parse_rbr_download_date.R
@@ -11,6 +11,7 @@
 #'
 #' @examples
 #' dt <- parse_rbr_download_date('20141117_1240')
+#' dt <- parse_rbr_download_date('201adf41a11a7_1240')
 parse_rbr_download_date <- function(fn, tz = 'UTC') {
 
   ind <- regexpr('\\d{8}\\_\\d{4}', fn)
@@ -21,7 +22,7 @@ parse_rbr_download_date <- function(fn, tz = 'UTC') {
 
   # return NA if no match
   if(length(tm) == 0) {
-    return(NA)
+    return(as.POSIXct(NA))
   }
 
   return(tm)
