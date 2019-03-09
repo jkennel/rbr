e6e47aed5c5896fd23a57dda33b9d9aed49a0416
jkennel <jkennel@uoguelph.ca>
2019-03-09 09:33:49 -0500

    remove extra filter_dates

--- R/filter_dates.R
+++ R/filter_dates.R
@@ -122,25 +122,25 @@ find_nearest <- function(transducer, manual, roll_size = 86400*7) {
 
 
 
-#===============================================================================
-#' @title filter_dates
+#' #===============================================================================
+#' #' @title filter_dates
+#' #'
+#' #' @description remove values from a data.table (non-equi join filter)
+#' #'
+#' #' @author Jonathan Kennel \email{jkennel@uoguelph.ca}
+#' #'
+#' #' @param dat data.table to filter (name, datetime)
+#' #' @param filt data.table of filter start and end times (start, end)
+#' #'
+#' #' @return filtered data.table
+#' #'
+#' #' @export
+#' #===============================================================================
+#' filter_dates <- function(dat, filt){
 #'
-#' @description remove values from a data.table (non-equi join filter)
+#'   dat[!J(filt), on = .(name, datetime >= start, datetime <= end)]
 #'
-#' @author Jonathan Kennel \email{jkennel@uoguelph.ca}
-#'
-#' @param dat data.table to filter (name, datetime)
-#' @param filt data.table of filter start and end times (start, end)
-#'
-#' @return filtered data.table
-#'
-#' @export
-#===============================================================================
-filter_dates <- function(dat, filt){
-
-  dat[!J(filt), on = .(name, datetime >= start, datetime <= end)]
-
-}
+#' }
 
 
 
